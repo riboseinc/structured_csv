465a25781c2b2cc89328455bea552993ee364a98
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-05-14 15:55:25 +0800

    spec: Add helpers
     Conflicts:
    	lib/structured_csv.rb
    	spec/spec_helper.rb

---
+++ spec/support/structured_csv_helper.rb
@@ -0,0 +1,10 @@
+require 'tmpdir'
+
+module StructuredCsv
+  module Helper
+    def create_tmp_dir
+      dir = Dir.mktmpdir("structured_csv")
+      Dir.glob(dir).first # expand the ~1 suffix on Windows
+    end
+  end
+end
