b193b0446f08b8ab31bc2ae3bf366b84cb1cc0ab
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-05-14 15:58:49 +0800

    spec: Add some tests

---
+++ spec/structured_csv/csv_join_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+RSpec.describe StructuredCsv::CsvJoin do
+  describe '.join' do
+    let(:command) { described_class.method(:join) }
+    let(:csv) {
+      CSV.parse(
+      <<~EOF
+      a,b,c
+      d,e,f
+EOF
+      )
+    }
+    let(:section_name) { 'test_section' }
+
+    # TODO: add more tests
+    it 'joins csv with given section_name' do
+      expect( command.call(csv, section_name) ).to_not be_nil
+    end
+  end
+
+  describe '.load_csv' do
+    let(:command) { described_class.method(:load_csv) }
+    let(:csv_content) {
+      <<~EOF
+      a,b,c
+      d,e,f
+EOF
+    }
+    let(:tmp_dir) { create_tmp_dir }
+    let(:csv_file) {
+      name = File.join(tmp_dir, 'temp.csv')
+      File.write(name, csv_content)
+      name
+    }
+
+    it 'reads the CSV' do
+      expect(command.call(csv_file)).to_not be_nil
+    end
+
+    it 'parses the CSV' do
+      expect(command.call(csv_file)).to eql [%w[a b c], %w[d e f]]
+    end
+
+  end
+end
